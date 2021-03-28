683f98529203b651d3c7e5f236114f2f1f0e253d
Serge Satansson <thornonthehead@gmail.com>
2021-03-28 15:05:15 +0300

    Initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 25.0f;
+    private float turnSpeed = 25f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        if (verticalInput == 0)
+            horizontalInput = 0;
+        if (verticalInput < 0)
+            horizontalInput *= -1;
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        
+    }
+}
